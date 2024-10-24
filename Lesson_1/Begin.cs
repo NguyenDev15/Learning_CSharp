6553da4df442fa29d024326caad01c03b25df03f
NguyenDev15 <nguyenn13122005@gmail.com>
2024-10-24 23:31:52 +0700

    Print Out

---
+++ Lesson_1/Begin.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lesson_1
+{
+    public class Begin
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Helloooo"); // không xuống dòng
+            Console.WriteLine("hé lô"); // xuống dòng
+            Console.ReadLine(); 
+            Console.ReadKey();
+            Console.WriteLine("Họ và tên");
+            Console.WriteLine("Năm sinh");
+            Console.WriteLine("Giới tính");
+            Console.WriteLine("Tuổi của bạn");
+        }
+    }
+}
