77a0cc8d3efa7633a30e8df9fd9a907ccb47221c
NguyenDev15 <nguyenn13122005@gmail.com>
2024-10-24 17:11:29 +0700

    first commit

---
+++ Frist/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Frist
+{
+    class Program{
+        public static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 1;
+            int c = a + b;
+            Console.WriteLine(c);
+        }
+    }
+}
+
