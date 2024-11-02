ee637fa4d35de94289a0933e9f6ec2023fb85cf7
NguyenDev15 <nguyenn13122005@gmail.com>
2024-11-03 02:07:04 +0700

    Update String Exercise

--- Lesson_21/obj/Debug/net8.0/Lesson_21.AssemblyInfo.cs
+++ Lesson_21/obj/Debug/net8.0/Lesson_21.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lesson_21")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+236ce7de0fa08b2853db3a3d74c20fad726ebf53")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b72de6b59e3709191fa711047c78feae8f9dc7fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lesson_21")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lesson_21")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
